7f804ab5adfdb3817f526550a038f74b5789b829
Fox <hyxand4503@163.com>
2021-08-31 20:53:21 +0800

    规格

--- app/src/main/java/com/lingmiao/shop/business/goods/SpecSettingActivity.kt
+++ app/src/main/java/com/lingmiao/shop/business/goods/SpecSettingActivity.kt
@@ -107,9 +107,9 @@ class SpecSettingActivity : BaseActivity<SpecSettingPre>(),
         if(skuList == null || specKeyList == null) {
             mPresenter.loadSpecKeyList(goodsId)
         }
-//        if(goodsId == null) {
-//            mPresenter.loadSpecListByCid(categoryId);
-//        }
+        if(skuList == null && specKeyList == null && goodsId == null) {
+            mPresenter.loadSpecListByCid(categoryId);
+        }
     }
 
     private fun initAdapter() {
