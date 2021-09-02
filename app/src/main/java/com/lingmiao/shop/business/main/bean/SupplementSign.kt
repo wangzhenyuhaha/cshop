3199748612ad353cbd09624f3e62d3a7c533d3d2
Fox <hyxand4503@163.com>
2021-09-02 15:53:06 +0800

    补充信息

---
+++ app/src/main/java/com/lingmiao/shop/business/main/bean/SupplementSign.kt
@@ -0,0 +1,9 @@
+package com.lingmiao.shop.business.main.bean
+
+
+import com.google.gson.annotations.SerializedName
+
+data class SupplementSign(
+    @SerializedName("url")
+    var url: String? = ""
+)
