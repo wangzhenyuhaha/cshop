293c1d6198fd9bddd8065fcfc49555f85ff63451
wangzhenyu <2903981314@qq.com>
2022-01-07 17:47:29 +0800

    test01

--- app/src/main/java/com/lingmiao/shop/business/main/fragment/NewMainFragment.kt
+++ app/src/main/java/com/lingmiao/shop/business/main/fragment/NewMainFragment.kt
@@ -487,8 +487,8 @@ class NewMainFragment : BaseFragment<MainPresenter>(), MainPresenter.View {
 //             GoodsPublishActivity.openActivity(context!!, "");
         }
         testNewFra.setOnClickListener {
-            //ActivityUtils.startActivity(SalesMarketingActivity::class.java)
-            ActivityUtils.startActivity(DiscountActivity::class.java)
+            ActivityUtils.startActivity(SalesMarketingActivity::class.java)
+           // ActivityUtils.startActivity(DiscountActivity::class.java)
         }
         // 营销设置
         tvSaleSetting.setOnClickListener {
--- app/src/main/java/com/lingmiao/shop/business/sales/adapter/DiscountAdapter.kt
+++ app/src/main/java/com/lingmiao/shop/business/sales/adapter/DiscountAdapter.kt
@@ -10,7 +10,17 @@ class DiscountAdapter :
 
 
     override fun convert(helper: BaseViewHolder, item: Coupon?) {
-        helper.setText(R.id.orderGoodsNameTv, item?.title)
+        //优惠券面额
+        helper.setText(R.id.jianPrice, "¥ ${item?.jianPrice.toString()}")
+        //优惠券门槛
+        helper.setText(R.id.manPrice, "满${item?.manPrice.toString()}可用")
+        //优惠券名字
+        helper.setText(R.id.title, item?.title)
+        //优惠券库存
+        helper.setText(R.id.createNum, "（库存${item?.createNum}）")
+        //优惠券状态
+        // helper.setText(R.id.orderGoodsNameTv, item?.title)
+        //优惠券
     }
 
 }
