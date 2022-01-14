ae9358380bed357630f6f3d4f5c31dd86e17d4d4
wangzhenyu <2903981314@qq.com>
2022-01-14 14:51:03 +0800

    满减活动增加

--- app/src/main/java/com/lingmiao/shop/business/sales/SalesActivityEditActivity.kt
+++ app/src/main/java/com/lingmiao/shop/business/sales/SalesActivityEditActivity.kt
@@ -27,29 +27,29 @@ Desc        :
  **/
 class SalesActivityEditActivity : BaseActivity<ISalesEditPresenter>(), ISalesEditPresenter.PubView {
 
-    var pvCustomTime : TimePickerView?= null;
-    var pvCustomTime2 : TimePickerView? = null;
+    var pvCustomTime: TimePickerView? = null
+    var pvCustomTime2: TimePickerView? = null
 
-    var mItem : SalesVo? = null;
-    var mViewType : Int? = 0
+    var mItem: SalesVo? = null
+    var mViewType: Int? = 0
 
-    private lateinit var mDiscountList : MutableList<SalesActivityItemVo>;
+    private lateinit var mDiscountList: MutableList<SalesActivityItemVo>
 
-    private lateinit var mDiscountAdapter : ActivitySalesAdapter;
+    private lateinit var mDiscountAdapter: ActivitySalesAdapter
 
     companion object {
 
         const val KEY_ITEM = "KEY_ITEM"
         const val KEY_VIEW_TYPE = "KEY_VIEW_TYPE"
-        const val VALUE_ALL = 1;
-        const val VALUE_PART = 2;
-        val REQUEST_GOODS = 299;
+        const val VALUE_ALL = 1
+        const val VALUE_PART = 2
+        val REQUEST_GOODS = 299
 
-        fun open(context: Activity, result : Int) {
-            edit(context, null, result);
+        fun open(context: Activity, result: Int) {
+            edit(context, null, result)
         }
 
-        fun edit(context: Activity, item: SalesVo?, result : Int) {
+        fun edit(context: Activity, item: SalesVo?, result: Int) {
             val intent = Intent(context, SalesActivityEditActivity::class.java)
             intent.putExtra(KEY_ITEM, item)
             context.startActivityForResult(intent, result)
@@ -64,197 +64,195 @@ class SalesActivityEditActivity : BaseActivity<ISalesEditPresenter>(), ISalesEdi
 
     }
 
-
     override fun initBundles() {
-        mItem = intent?.getSerializableExtra(KEY_ITEM) as SalesVo?;
-        mViewType = intent?.getIntExtra(KEY_VIEW_TYPE, 0);
+        mItem = intent?.getSerializableExtra(KEY_ITEM) as SalesVo?
+        mViewType = intent?.getIntExtra(KEY_VIEW_TYPE, 0)
     }
 
-    override fun createPresenter(): ISalesEditPresenter {
-        return SalesActivityEditPreImpl(this, this);
-    }
+    override fun createPresenter() = SalesActivityEditPreImpl(this, this)
 
-    override fun getLayoutId(): Int {
-        return R.layout.sales_activity_activity_edit;
-    }
 
-    override fun useLightMode(): Boolean {
-        return false;
-    }
+    override fun getLayoutId() = R.layout.sales_activity_activity_edit
+
+
+    override fun useLightMode() = false
+
 
     override fun initView() {
         mToolBarDelegate.setMidTitle(getString(R.string.sales_activity_edit_title))
 
-
-
-        initPricePart();
+        initPricePart()
 
         initDate()
 
-        if(mViewType == VALUE_PART) {
+        if (mViewType == VALUE_PART) {
             submitTv.gone()
         } else {
             submitTv.visiable()
             activityGoodsPickTv.singleClick {
-                SalesGoodsActivity.edit(context, mItem, REQUEST_GOODS);
+                SalesGoodsActivity.edit(context, mItem, REQUEST_GOODS)
             }
         }
         submitTv.singleClick {
+
+            if (mDiscountAdapter.data.size == 0 || mDiscountAdapter.data[0] == null) {
+                showToast("请输入满减金额")
+                return@singleClick
+            }
             try {
-                checkNotBlack(menuNameEdt.getViewText()) { "请输入活动名称" };
+                checkBoolean(firstMenuTv.getViewText() != "请选择") { "请输入活动开始时间" }
 
-                checkBoolean(firstMenuTv.getViewText() != "请选择") { "请输入活动开始时间" };
-
-                checkBoolean(secondMenuTv.getViewText() != "请选择") { "请输入活动开始时间" };
+                checkBoolean(secondMenuTv.getViewText() != "请选择") { "请输入活动开始时间" }
 
             } catch (e: BizException) {
                 showToast(e.message.check())
-                return@singleClick;
+                return@singleClick
             }
 
-            if(mDiscountAdapter.data.size == 0 || mDiscountAdapter.data[0] == null) {
-                showToast("请输入满减金额")
-                return@singleClick;
-            }
-            if(mItem?.rangeType == null || mItem?.rangeType == 0) {
+
+            if (mItem?.rangeType == null || mItem?.rangeType == 0) {
                 showToast("请选择参与活动的商品")
-                return@singleClick;
+                return@singleClick
             }
 
-            mItem?.title = menuNameEdt.getViewText()
-            val s = string2Date(firstMenuTv.getViewText(), MINUTES_TIME_FORMAT)?.time?:0;
-            val e = string2Date(secondMenuTv.getViewText(), MINUTES_TIME_FORMAT)?.time?:0;
-            mItem?.startTime = s/1000;
-            mItem?.endTime = e/1000;
+            val s = string2Date(firstMenuTv.getViewText(), MINUTES_TIME_FORMAT)?.time ?: 0
+            val e = string2Date(secondMenuTv.getViewText(), MINUTES_TIME_FORMAT)?.time ?: 0
+            mItem?.startTime = s / 1000
+            mItem?.endTime = e / 1000
 
             mItem?.fullType = "MONEY"
-            val it = mDiscountAdapter.data.get(0);
-            mItem?.fullMoney = it.peach;
+            val it = mDiscountAdapter.data[0]
+            mItem?.fullMoney = it.peach
             mItem?.minusValue = it.least
 
-            if(mItem?.id?.isNotEmpty() ==true) {
-                mPresenter?.update(mItem);
+            mItem?.title = "满 ${it.peach} 减 ${it.least}"
+
+            if (mItem?.id?.isNotEmpty() == true) {
+                mPresenter?.update(mItem)
             } else {
-                mPresenter?.submitDiscount(mItem);
+                mPresenter?.submitDiscount(mItem)
             }
         }
 
 
-        if(mItem != null) {
-            resetUi();
+        if (mItem != null) {
+            resetUi()
         } else {
-            mItem = SalesVo();
-            mDiscountList.add(SalesActivityItemVo());
-            mDiscountAdapter.replaceData(mDiscountList);
+            mItem = SalesVo()
+            mDiscountList.add(SalesActivityItemVo())
+            mDiscountAdapter.replaceData(mDiscountList)
         }
     }
 
-    fun resetUi() {
+    private fun resetUi() {
         menuNameEdt.setText(mItem?.title)
-        firstMenuTv.setText(stampToDate(mItem?.startTime, MINUTES_TIME_FORMAT))
-        secondMenuTv.setText(stampToDate(mItem?.endTime, MINUTES_TIME_FORMAT))
-        mDiscountAdapter?.addData(mItem?.convertDiscountItem()!!);
-        setGoodsUi();
+        firstMenuTv.text = stampToDate(mItem?.startTime, MINUTES_TIME_FORMAT)
+        secondMenuTv.text = stampToDate(mItem?.endTime, MINUTES_TIME_FORMAT)
+        mDiscountAdapter.addData(mItem?.convertDiscountItem()!!)
+        setGoodsUi()
 
-        if(mItem?.status.equals("UNDERWAY")) {
-            submitTv.gone();
+        if (mItem?.status.equals("UNDERWAY")) {
+            submitTv.gone()
         }
 
     }
 
     private fun initPricePart() {
-        mDiscountList = arrayListOf();
+        mDiscountList = arrayListOf()
         mDiscountAdapter = ActivitySalesAdapter().apply {
             setOnItemChildClickListener { adapter, view, position ->
-                val item = adapter.data[position] as SalesActivityItemVo;
+                val item = adapter.data[position] as SalesActivityItemVo
                 if (view.id == R.id.discountDeleteTv && position != 0) {
-                    mDiscountList.remove(item);
-                    mDiscountAdapter.replaceData(mDiscountList);
+                    mDiscountList.remove(item)
+                    mDiscountAdapter.replaceData(mDiscountList)
                 }
             }
+        }
 
-//            val footer = View.inflate(context, R.layout.sales_footer_activity_add, null);
-//            footer.findViewById<View>(R.id.activityAddTv).setOnClickListener {
-//                mDiscountList.add(SalesActivityItemVo());
-//                mDiscountAdapter.replaceData(mDiscountList);
-//            }
-//            addFooterView(footer)
-        };
-
-        activityRv.initAdapter(mDiscountAdapter);
-        mDiscountAdapter.replaceData(mDiscountList);
+        activityRv.initAdapter(mDiscountAdapter)
+        mDiscountAdapter.replaceData(mDiscountList)
     }
 
-    fun initDate() {
+    private fun initDate() {
         // 系统当前时间
         val selectedDate: Calendar = Calendar.getInstance()
         val startDate: Calendar = Calendar.getInstance()
-        startDate.set(selectedDate.get(Calendar.YEAR), selectedDate.get(Calendar.MONTH), selectedDate.get(Calendar.DATE))
+        startDate.set(
+            selectedDate.get(Calendar.YEAR),
+            selectedDate.get(Calendar.MONTH),
+            selectedDate.get(Calendar.DATE)
+        )
 
         val endDate: Calendar = Calendar.getInstance()
-        endDate.set(selectedDate.get(Calendar.YEAR )+ 5, selectedDate.get(Calendar.MONTH), selectedDate.get(Calendar.DATE))
+        endDate.set(
+            selectedDate.get(Calendar.YEAR) + 5,
+            selectedDate.get(Calendar.MONTH),
+            selectedDate.get(Calendar.DATE)
+        )
 
 
         firstMenuTv.singleClick {
             KeyboardUtils.hideSoftInput(it)
             //时间选择器 ，自定义布局
-            pvCustomTime = getDefaultTimePicker(this, selectedDate, startDate, endDate , { date, view ->
-                firstMenuTv.text = formatString(date, MINUTES_TIME_FORMAT)
-                //firstMenuTv.setText(formatDateTime(date))
-            }, {
-                pvCustomTime?.returnData()
-                pvCustomTime?.dismiss()
-            }, {
-                pvCustomTime?.dismiss()
-            });
-            pvCustomTime?.show();
+            pvCustomTime =
+                getDefaultTimePicker(this, selectedDate, startDate, endDate, { date, view ->
+                    firstMenuTv.text = formatString(date, MINUTES_TIME_FORMAT)
+                    //firstMenuTv.setText(formatDateTime(date))
+                }, {
+                    pvCustomTime?.returnData()
+                    pvCustomTime?.dismiss()
+                }, {
+                    pvCustomTime?.dismiss()
+                })
+            pvCustomTime?.show()
         }
 
         secondMenuTv.singleClick {
             KeyboardUtils.hideSoftInput(it)
             //时间选择器 ，自定义布局
-            pvCustomTime2 = getDefaultTimePicker(this, selectedDate, startDate, endDate , { date, view ->
-                secondMenuTv.setText(formatString(date, MINUTES_TIME_FORMAT))
-            }, {
-                pvCustomTime2?.returnData()
-                pvCustomTime2?.dismiss()
-            }, {
-                pvCustomTime2?.dismiss()
-            });
-            pvCustomTime2?.show();
+            pvCustomTime2 =
+                getDefaultTimePicker(this, selectedDate, startDate, endDate, { date, view ->
+                    secondMenuTv.setText(formatString(date, MINUTES_TIME_FORMAT))
+                }, {
+                    pvCustomTime2?.returnData()
+                    pvCustomTime2?.dismiss()
+                }, {
+                    pvCustomTime2?.dismiss()
+                })
+            pvCustomTime2?.show()
         }
     }
 
     override fun onSubmitDiscount() {
         setResult(Activity.RESULT_OK)
-        finish();
+        finish()
     }
 
     override fun onUpdate() {
         setResult(Activity.RESULT_OK)
-        finish();
+        finish()
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
         if (resultCode == Activity.RESULT_OK) {
-            if(requestCode == REQUEST_GOODS) {
-                if(mItem == null) {
-                    mItem = SalesVo();
+            if (requestCode == REQUEST_GOODS) {
+                if (mItem == null) {
+                    mItem = SalesVo()
                 }
 
-                mItem?.rangeType = data?.getIntExtra("type", VALUE_ALL);
-                mItem?.goodsList = data?.getSerializableExtra("goodsList") as ArrayList<GoodsVO>?;
-                setGoodsUi();
+                mItem?.rangeType = data?.getIntExtra("type", VALUE_ALL)
+                mItem?.goodsList = data?.getSerializableExtra("goodsList") as ArrayList<GoodsVO>?
+                setGoodsUi()
             }
         }
     }
 
     fun setGoodsUi() {
-        var str = "请选择参与活动的商品";
-        when(mItem?.rangeType) {
+        var str = "请选择参与活动的商品"
+        when (mItem?.rangeType) {
             VALUE_ALL -> {
-                str = "全部商品参与";
+                str = "全部商品参与"
             }
             VALUE_PART -> {
                 str = "部分商品不参与";
